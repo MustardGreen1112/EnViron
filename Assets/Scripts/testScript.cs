c1f4647ca7ba161f79c618cb74301ff33ad42ccd
CS B007 Lab Windows <cslabwin@CSB7-BHN9RT3.kiewit.dartmouth.edu>
2025-10-29 18:45:13 -0400

    testing chase commit

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("testing Chase ability to commit to repo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
