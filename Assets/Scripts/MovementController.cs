24e96c808b23669761f25a910b98d1ce858049c2
Anastasia Johnson <riley.c.johnson.25@dartmouth.edu>
2025-10-31 00:20:41 -0400

    Changed position to localPosition
    Script previously was tracking world position of model virus, thereby compounding a small change in position to a huge amount of acceleration

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -8,13 +8,13 @@ public class MovementController : MonoBehaviour
 
     void Start()
     {
-        startingPosition = modelVirus.GetComponent<Transform>().position;
+        startingPosition = modelVirus.GetComponent<Transform>().localPosition;
         rigid = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        Vector3 displacement = modelVirus.GetComponent<Transform>().position - startingPosition;
+        Vector3 displacement = modelVirus.GetComponent<Transform>().localPosition - startingPosition;
 
         // Move this object according to the displacement of the model virus
         rigid.AddForce(displacement, ForceMode.Acceleration);
