fdca70547fefcbbe0f6fd9375b24dffde3e1580c
Anastasia Johnson <riley.c.johnson.25@dartmouth.edu>
2025-11-10 23:43:10 -0500

    Virus particle generator
    Created a particle spawner that can be activated or deactivated by calling the TurnOnVirusGen() method, and similarly TurnOffVirusGen() to turn it off. Along with this is a trigger volume with an OnTriggerEnter method to call TurnOnVirusGen()

---
+++ Assets/Scripts/VirusGenTrigger.cs
@@ -0,0 +1,20 @@
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class VirusGenTrigger : MonoBehaviour
+{
+    VirusGenController virusGen;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        virusGen = transform.parent.gameObject.GetComponent<VirusGenController>();
+    }
+
+    // OnTriggerEnter is called when this object's Collider overlaps with another Collider,
+    // at least one of them being set to trigger
+    void OnTriggerEnter(Collider other)
+    {
+        virusGen.TurnOnVirusGen();
+    }
+}
